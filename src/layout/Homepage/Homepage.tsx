30745b2586ecfee246642d5d4b641ad0ddfad207
Vitaliy <58368192+VitaliyLomachinsky@users.noreply.github.com>
2023-10-10 10:11:28 +0300

    Remove Slider

--- src/layout/Homepage/Homepage.tsx
+++ src/layout/Homepage/Homepage.tsx
@@ -1,16 +1,9 @@
-import Slider from "../../components/Slider/Slider";
+
 
 const Homepage = () => {
     return <div className="homepage">
         <h2>Homepage</h2>
-        <Slider>
-            <div className="slider slider_1">Slider 1</div>
-            <div className="slider slider_2">Slider 2</div>
-            <div className="slider slider_3">Slider 3</div>
-            <div className="slider slider_1">Slider 4</div>
-            <div className="slider slider_2">Slider 5</div>
-            <div className="slider slider_3">Slider 6</div>
-        </Slider>
+
     </div>
 }
 
