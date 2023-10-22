542d184435833d30618522421e711e5ddaed2de9
Vitaliy <58368192+VitaliyLomachinsky@users.noreply.github.com>
2023-10-22 23:36:40 +0300

    tab ui change

--- src/Redux/Slices/UIReducer.tsx
+++ src/Redux/Slices/UIReducer.tsx
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 interface UIState {
     basket: boolean;
     search: boolean;
+    tab: string;
 }
 
 const initialState: UIState = {
-    basket: false,  
+    basket: false,
     search: false,
+    tab: "catalog",
 }
 
 export const UIReducer = createSlice({
@@ -20,10 +22,13 @@ export const UIReducer = createSlice({
         search_open: (state) => {
             state.search = !state.search;
         },
+        set_tab: (state, action: PayloadAction<string>) => {
+            state.tab = action.payload;
+        }
 
     },
 })
 
-export const { basket_open, search_open } = UIReducer.actions
+export const { basket_open, search_open,set_tab } = UIReducer.actions
 
 export default UIReducer.reducer;
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -2,18 +2,31 @@ import { BsFillBasketFill, BsSearch } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
 import Search_and_basket from "./Components/Search_and_basket";
+import { useAppDispatch, useAppSelector } from "../../Redux/hook";
+import { set_tab } from "../../Redux/Slices/UIReducer";
 
 
 const Header = () => {
+
+    const tab = useAppSelector((state => state.UI.tab));
+    const dispatch = useAppDispatch();
 
 
     return <header className="header">
         <Link to="/"><h1 className="header_title">Sneakers-Revolution</h1></Link>
         <ul className="header_navigate">
-            <Link to="/catalog"><li>Catalog</li></Link>
-            <Link to="/new_release"> <li>Release</li></Link>
-            <Link to="/contact"> <li>Contact</li></Link>
-            <Link to="/about"> <li className="active_header_navigate">About</li></Link>
+            <Link to="/catalog"><li
+                className={tab == "catalog" ? "active_header_navigate" : ""}
+                onClick={() => dispatch(set_tab("catalog"))}>Catalog</li></Link>
+            <Link to="/new_release"> <li
+                className={tab == "relese" ? "active_header_navigate" : ""}
+                onClick={() => dispatch(set_tab("relese"))}>Release</li></Link>
+            <Link to="/contact"> <li
+                className={tab == "contact" ? "active_header_navigate" : ""}
+                onClick={() => dispatch(set_tab("contact"))}>Contact</li></Link>
+            <Link to="/about"> <li
+                className={tab == "about" ? "active_header_navigate" : ""}
+                onClick={() => dispatch(set_tab("about"))}>About</li></Link>
         </ul>
         <Search_and_basket />
     </header>
